ded960743df58b7a1a02da2b44fe3e6521cf4ff4
PriyanshuPaul <priyanshupaul23@gmail.com>
2022-04-10 18:48:33 +0530

    Adding buttons to post section

--- src/features/feed/post-option/PostOption.js
+++ src/features/feed/post-option/PostOption.js
@@ -1,6 +1,12 @@
 import { Avatar } from "@material-ui/core"
 import React from "react"
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import MessageIcon from '@material-ui/icons/Message';
+import ShareIcon from '@material-ui/icons/Share';
+import SendIcon from '@material-ui/icons/Send';
+
 import './PostOption.css'
+import InputOption from "../input-option/InputOption";
 function PostOption({name,description,message,photo}) {
   return (
     <div className="post">
@@ -13,6 +19,13 @@ function PostOption({name,description,message,photo}) {
         </div>
         <div className="post-body">
             <p>{message}</p>
+        </div>
+        <div className="post-button">
+          <InputOption Icon={ThumbUpAltIcon} title="Like" color="#66b3ff"/>
+          <InputOption Icon={MessageIcon} title="Comment" color="#00b300"/>
+          <InputOption Icon={ShareIcon} title="Share" color="#ffb84d"/>
+          <InputOption Icon={SendIcon} title="Send" color="#ff99bb"/>
+
         </div>
     </div>
   )
